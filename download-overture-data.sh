dc794d9e2a0ff5d0e9c0920f2666303838a793f8
Lê Khải Hoàn <hoanlk@craftzlife.com>
2026-03-04 16:20:11 +0700

    refactor: streamline data download process with loop and spinner function

--- download-overture-data.sh
+++ download-overture-data.sh
@@ -3,21 +3,37 @@ LOCATION="ho_chi_minh_city"
 
 mkdir -p ./data/raw
 
-overturemaps download --bbox=$BBOX -f geojson --type=building -o ./data/raw/${LOCATION}_building.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=address -o ./data/raw/${LOCATION}_address.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=bathymetry -o ./data/raw/${LOCATION}_bathymetry.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=building_part -o ./data/raw/${LOCATION}_building_part.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=division -o ./data/raw/${LOCATION}_division.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=division_area -o ./data/raw/${LOCATION}_division_area.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=division_boundary -o ./data/raw/${LOCATION}_division_boundary.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=place -o ./data/raw/${LOCATION}_place.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=segment -o ./data/raw/${LOCATION}_segment.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=connector -o ./data/raw/${LOCATION}_connector.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=infrastructure -o ./data/raw/${LOCATION}_infrastructure.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=land -o ./data/raw/${LOCATION}_land.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=land_cover -o ./data/raw/${LOCATION}_land_cover.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=land_use -o ./data/raw/${LOCATION}_land_use.geojson
-overturemaps download --bbox=$BBOX -f geojson --type=water -o ./data/raw/${LOCATION}_water.geojson
+spin() {
+  local pid=$1
+  local label=$2
+  local chars="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+  local i=0
+  while kill -0 "$pid" 2>/dev/null; do
+    printf "\r  %s  %s" "${chars:i%${#chars}:1}" "$label"
+    i=$((i + 1))
+    sleep 0.1
+  done
+  wait "$pid"
+  local rc=$?
+  if [ $rc -eq 0 ]; then
+    printf "\r  ✔  %s\n" "$label"
+  else
+    printf "\r  ✖  %s\n" "$label"
+    return $rc
+  fi
+}
+
+TYPES=(
+  building address bathymetry building_part division division_area
+  division_boundary place segment connector infrastructure land
+  land_cover land_use water
+)
+
+for type in "${TYPES[@]}"; do
+  overturemaps download --bbox=$BBOX -f geojson --type=$type -o ./data/raw/${LOCATION}_${type}.geojson &>/dev/null &
+  spin $! "Download $type"
+done
 
 S3_BUCKET="overturemapsgeospatialdatastack-databuckete3889a50-ybo0e2chjflg"
-aws --profile 101 s3 sync ./data/ s3://${S3_BUCKET}/
+aws --profile 101 s3 sync ./data/ s3://${S3_BUCKET}/ &>/dev/null &
+spin $! "Upload to S3"
